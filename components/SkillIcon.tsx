9a5b1681d2112265e5cdc03e0c10b143a7aca31c
sandrineingabire2 <ingasandrine031@gmail.com>
2025-12-16 14:09:32 +0200

    deploy debug 2

--- components/SkillIcon.tsx
+++ components/SkillIcon.tsx
@@ -1,5 +1,7 @@
+import React from 'react';
+
 export default function SkillIcon({ name }: { name: string }) {
-  const icons: { [key: string]: JSX.Element } = {
+  const icons: { [key: string]: React.ReactElement } = {
     'HTML': (
       <svg className="w-12 h-12" viewBox="0 0 24 24" fill="currentColor">
         <path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.564-2.438L1.5 0zm7.031 9.75l-.232-2.718 10.059.003.23-2.622L5.412 4.41l.698 8.01h9.126l-.326 3.426-2.91.804-2.955-.81-.188-2.11H6.248l.33 4.171L12 19.351l5.379-1.443.744-8.157H8.531z"/>
